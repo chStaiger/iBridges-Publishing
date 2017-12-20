166ec6c8e95df4f12a5c4a6edc1ee7f5c95f3c48
chStaiger <christine.staiger@surfsara.nl>
2017-12-20 17:44:51 +0100

    Adjust to new classes.

--- clients/irodsRepositoryClient.py
+++ clients/irodsRepositoryClient.py
@@ -1,37 +1,48 @@
 from irods.access import iRODSAccess 
 
-from b2handle.handleclient import EUDATHandleClient
-from b2handle.clientcredentials import PIDClientCredentials
+RED     = "\033[31m"
+GREEN   = "\033[92m"
+BLUE    = "\033[34m"
+DEFAULT = "\033[0m"
 
 class irodsRepositoryClient():
 
-    def __init__(self, irodspublishcoll, draft, pids = {}, tickets = {}, pidClient = ''):
+    def __init__(self, irodscoll, draft, pidClient = ''):
 
         self.draft = draft
         self.ipc = irodspublishcoll
         print "Publish collection: ", self.ipc.coll.path
         print "Draft ID: ", self.draftId
-        self.pids = pids
-        self.tickets = tickets
-        self.pidClient = picClient
+        self.pids = ipc.getMDall(self, 'PID')
+        self.tickets = ipc.getMDall(self, 'TICKET')
+        self.pidClient = pidClient
 
-    def checkCollection(pids = true, tickets = true):
+    def checkCollection(self, pids = true, tickets = true):
         
         message = []
         #Check if flat directory and no DOI from repository (unpublished)
         message.extend(self.ipc.validate(repoKeys = [self.draft.repoName + '/DOI']))
 
         #Check if mandatory metadata is present
-        if not set(ipc.mp.keys()).issubset(draft.metaKeys):
-            message.append('REPOSITORY PUBLISH ERROR: Keys not defined: ' + str(set(draft.metaKeys).difference(ipc.md.keys()))
-
-        #Create PIDs, if not done by eventhook in iRODS
+        if not set(self.ipc.mp.keys()).issubset(self.draft.metaKeys):
+            message.append(self.draft.repoName + ' PUBLISH ERROR: Keys not defined: ' + \
+                str(set(self.draft.metaKeys).difference(self.ipc.md.keys()))
+        else:
+            message.append(self.draft.repoName + ' PUBLISH NOTE: all metadata defined: ' +\
+                str(self.draft.metaKeys))
+                           
+        #Create PIDs, if not present
         if pids and self.pids == {}:
-            self.pids = ipc.assignPids(self.pidClient) 
+            self.pids = self.ipc.assignPID(ec)
+        message.extend(['PIDs for collection: ', str(self.pids)])
 
-        #Create tickets for anonymous download of data from iRODS, if data is not uploaded to repository
-        if tickets and self.tickets == {}:
-            self.tickets = ipc.assignTickets()
+        #Create tickets for anonymous download of data from iRODS if not present
+        if tickets and self.ipc.tickets = {}: 
+            self.tickets, error = self.ipc.assignTicket()
+            if error:
+                message.extend(error)
+                print RED + 'Assigning tickets failed' + DEFAULT
+        message.extend(['Tickets for collection', str(self.tickets)])
 
         return message
 
@@ -42,14 +53,14 @@ class irodsRepositoryClient():
 
         path = path + "/" + self.ipc.coll.name
         try:
-            print "Remove old data: ", path
             shutil.rmtree(path)
         except OSError:
             print path, "does not exist."
+                           
         os.makedirs(path)
 
-        for obj in self.coll.data_objects:
-            buff = self.session.data_objects.open(obj.path, 'r').read()
+        for obj in self.ipc.coll.data_objects:
+            buff = self.ipc.session.data_objects.open(obj.path, 'r').read()
             with open(path+'/'+obj.name, 'wb') as f:
                 f.write(buff)
 
@@ -58,28 +69,34 @@ class irodsRepositoryClient():
     def uploadToRepo(self, data=True):
 
         message = []
-        message.extend(draft.patchGeneral(self.ipc.metadata))
+        message.extend(self.draft.create(self.ipc.md['TITLE']))
+        message.extend(self.draft.patchGeneral(self.ipc.metadata))
         if tickets != {}:
-            message.extend(draft.patchTickets(self.tickets))
+            message.extend(self.draft.patchTickets(self.tickets))
         if tickets != {}:
-            message.extend(draft.patchPIDs(self.pids))
+            message.extend(self.draft.patchPIDs(self.pids))
 
         if data:
-            folder = localCopyData()
-            message.extend(uploadData(folder))        
+            folder = self.localCopyData()
+            message.extend(self.draft.uploadData(folder))        
 
         return message
-
-    def addPublishIDtoIRODS(self, doi, id=''):
+                         
+    def publishDraft(self):
         '''
+        Publishes a draft (checks only on draft.draftUrl).
         Adds a PID from the repository to a collection in iRODS.
         key:    repoName/DOI: doi
                 repoName/ID: id
         '''
-
-        self.ipc.mdUpdate(draft.repoName+'/DOI', doi)
-        if id != '':
-            self.ipc.mdUpdate(draft.repoName+'/ID', self.draftId)
+        assert self.draft.draftUrl != ''
+        
+        message = []
+        doi = self.draft.publish()   
+        message.append(self.ipc.mdUpdate(self.draft.repoName+'/DOI', doi))
+        message.append(self.ipc.mdUpdate(self.draft.repoName+"ID", self.draft.draftId))
+                           
+        return message                   
 
     def createReport(self, content, owners=[]):
         '''
